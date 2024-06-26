911dc1b458f012bff5a8a63547764432a820b1cb
Mahdy <youssef_mahdy@dell.com>
2024-06-26 18:29:51 +0300

    Added methods to check and build images

--- Keywords.py
+++ Keywords.py
@@ -1,7 +1,31 @@
+from robot.api import logger
 import paramiko
+import os
+import getpass
 
 class Keywords:
-    def ssh_to_server(self,ip_address, username, password, command):
+    ROBOT_LIBRARY_SCOPE = "GLOBAL"
+    def __init__(self, ip_address) -> None:
+        self.username = None
+        self.password = None
+        self.ssh_to_server(ip_address=ip_address)
+    
+    def add_credentials(self) -> None:
+        """
+        Adds credentials to the object by retrieving them from the environment variables or prompting the user for input.
+
+        :return: None
+        """
+        if self.username is None:
+            if os.environ.get("USERNAME"):
+                self.username = os.environ.get("USERNAME") 
+            else:
+                logger.info(f"Please input value for USERNAME:", also_console=True)
+                self.username = input()
+        if self.password is None:
+            self.password = os.environ.get("PASSWORD") if os.environ.get("PASSWORD") else getpass.getpass(f"Please input password:")
+
+    def ssh_to_server(self,ip_address) -> None:
         """
         SSH to a server and execute a command.
         
@@ -11,27 +35,17 @@ class Keywords:
         :param command: The command to execute on the server.
         :return: The output of the command.
         """
+        # if self.username is None or self.password is None:
+        self.add_credentials()
         try:
             # Create a new SSH client
-            ssh = paramiko.SSHClient()
+            self.ssh = paramiko.SSHClient()
 
             # Automatically add the server's public key to the known_hosts file
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
             # Connect to the server
-            ssh.connect(ip_address, username=username, password=password)
-            # Execute the command
-            stdin, stdout, stderr = ssh.exec_command(command)
-            # Get the command output and error
-            output = stdout.read().decode()
-            error = stderr.read().decode()
-            
-            # Close the SSH connection
-            ssh.close()
-            
-            if error:
-                return f"Error: {error}"
-            return output
+            self.ssh.connect(ip_address, username=self.username, password=self.password)
         
         except paramiko.AuthenticationException:
             raise Exception("Authentication failed, please verify your credentials")
@@ -39,4 +53,33 @@ class Keywords:
             raise Exception(f"Unable to establish SSH connection: {sshException}")
         except Exception as e:
             raise Exception(f"Exception in connecting to the server: {e}")
-
+    
+    def send_command(self, command) -> str:
+        try:
+            # Execute the command
+            stdin, stdout, stderr = self.ssh.exec_command(command)
+            # Get the command output and error
+            output = stdout.read().decode()
+            error = stderr.read().decode()
+            if output:
+                logger.info(output)
+                return output
+            else:
+                raise Exception(f"Failed due to error:{error}")
+        except Exception as e:
+            raise Exception("Failed to execute command due to error:" + str(e))
+        
+    def check_images(self, image):
+        try:
+            image_exists = self.send_command(f"podman images | grep {image} | wc -l")
+            return int(image_exists)
+        except Exception as e:
+            raise Exception(f"Failed to check Image {image}")
+    
+    def build_image(self, image, path_to_directory) -> None:
+        try:
+            self.send_command(f"cd {path_to_directory} && podman build -t {image}")
+        except Exception as e:
+            raise Exception(f"Failed to build image {image} due to error: {e}")
+    def close_session(self) -> None:
+        self.ssh.close()
--- Testing_Keywords.robot
+++ Testing_Keywords.robot
@@ -1,16 +1,18 @@
 *** Settings ***
 Documentation    This suite consists of testing the methods in Keywords
-Library          Keywords.py
-
-*** Variables ***
-${USERNAME}=    youssef_mahdy
-${PASSWORD}=    140996_Joe
-
+Library          Keywords.py   100.64.24.217
 
 *** Test Cases ***
 Test SSH To Server
     [Documentation]    Verify SSH To Server is working fine
     [Tags]    Milestone2
 
-    ${output}=    SSH To Server    10.237.95.2    ${USERNAME}    ${PASSWORD}    whoami
-    Log    ${output}
+    Send Command    whoami
+    ${back_ret_status} =    Check Images    academy_backend
+
+    Run Keyword IF  ${back_ret_status} < 1     Build Image     academy_backend    /root/simple-django-app
+    
+    ${front_ret_status} =    Check Images    academy_frontend
+
+    Run Keyword IF  ${front_ret_status} < 1     Build Image     academy_frontend    /root/simple-django-app
+
